f8c83da90b703db9fb8cbf5967cc09913e2394f8
OKEPlazmA <okeplazma@gmail.com>
2017-11-22 14:34:09 -0600

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -3,7 +3,7 @@ import modis
 DISCORD_TOKEN = "MzM5MjIwMjU4MDI2MTYwMTMx.DPdO5g.wvUi6sipKLHHWFXIkLr--3J2O_k"
 CLIENT_ID = "339220258026160131"
 
-modis.gui(
+modis.console(
     discord_token=DISCORD_TOKEN,
     discord_client_id=CLIENT_ID
 )
